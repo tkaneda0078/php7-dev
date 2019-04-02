9cf504d40254a119d62234f3e1bb6e17464e5c7d
tkaneda0078 <1111510078r21@gmail.com>
2019-04-02 20:58:58 +0900

    login画面の側作成

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST)) {
+
+}
+
+?>
+
+<html>
+  <body>
+    <form action="login.php" method="post">
+      <h1>Times Square</h1>
+      <input type="text" name="username" placeholder="Username"/>
+      <input type="password" name="password" placeholder="Password"/>
+      <button type="submit">Login</button>
+    </form>
+  </body>
+</html>
